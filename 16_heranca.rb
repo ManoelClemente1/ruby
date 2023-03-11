485a7dda07e827a0e8980f36a893d1c3cdbb11ce
ManoelClemente1 <manoelclemente9@outlook.com>
2023-03-11 18:52:11 -0300

    herança

---
+++ 16_heranca.rb
@@ -0,0 +1,35 @@
+class Pessoa
+    attr_accessor :nome, :email
+end
+
+class PessoaFisica < Pessoa
+    attr_accessor :cpf
+
+    def falar(texto)
+        texto
+    end
+end
+
+class PessoaJuridica < Pessoa
+    attr_accessor :cnpj
+
+    def pagar_fornecedor
+        puts "fornecedor pago"
+    end
+end
+
+p1 = Pessoa.new
+puts p1.nome = "Maneco"
+puts p1.email = "maneco.com"
+
+p2 = PessoaFisica.new
+puts p2.nome = "Maneco Pessoa Fisica"
+puts p2.email = "manecopfisico.com"
+puts p2.cpf = "132132132"
+p2.falar("Hello!")
+
+p3 = PessoaJuridica.new
+puts p3.nome = "Maneco Pessoa Juridica"
+puts p3.email = "manecopjuridica.com"
+puts p3.cnpj = "331321456454"
+p3.pagar_fornecedor
